91b96b6937a3d0073e006db0db445f12f6d1610e
Addison kessler <akessler1218@gmail.com>
2026-03-03 16:42:51 -0600

    Add files via upload

---
+++ e.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define e using
+#define ee namespace
+#define eee ;
+#define eeee cout
+#define eeeee <<
+#define eeeeee "Hello E"
+#define eeeeeee std
+#define eeeeeeee int
+#define eeeeeeeee main
+#define eeeeeeeeee return
+#define eeeeeeeeeee 0
+#define eeeeeeeeeeee endl
--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-#include <iostream>
-#define e using
-#define ee namespace
-#define eee ;
-#define eeee cout
-#define eeeee <<
-#define eeeeee "Hello E"
-#define eeeeeee std
-#define eeeeeeee int
-#define eeeeeeeee main
-#define eeeeeeeeee return
-#define eeeeeeeeeee 0
-#define eeeeeeeeeeee endl
+#include "e.hpp"
 e ee eeeeeee eee
 eeeeeeee eeeeeeeee(){
     eeee eeeee eeeeee eeeee eeeeeeeeeeee eee
